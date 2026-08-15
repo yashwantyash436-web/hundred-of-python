e88154a0f81c19ccfdc65a198cd149b641a95c58
Yashwanth <yashyashwant436@gmail.com>
2026-08-16 01:27:24 +0530

    day 32 completed

---
+++ 32/main.py
@@ -0,0 +1,68 @@
+# import smtplib
+# import datetime as dt
+# import pandas
+# import random
+
+
+# now=dt.datetime.now()
+# today=now.weekday()
+# print(today)
+# # date_of_birth =dt.datetime(year=2005, month=12, day=2)
+# # print(date_of_birth)
+
+# if today==5:
+    
+#     with open("quotes.txt") as file:
+#         quuotes_list=file.readlines()
+#         quotes=random.choice(quuotes_list)
+#     print(quotes)
+    
+#     my_email = "4vm24is048@gmail.com"
+#     password = "pzkpyuklywkwvfnd"
+
+#     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+#         connection.starttls()
+
+#         connection.login(user=my_email, password=password)
+
+#         connection.sendmail(
+#             from_addr=my_email,
+#             to_addrs="yashwantyash067@gmail.com",
+#             msg=f"Subject: Test Email\n\n{quotes}"
+#         )
+#To run and test the code you need to update 4 places:
+# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
+# 2. Go to your email provider and make it allow less secure apps.
+# 3. Update the SMTP ADDRESS to match your email provider.
+# 4. Update birthdays.csv to contain today's month and day.
+# See the solution video in the 100 Days of Python Course for explainations.
+
+
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
---
+++ 32/tempCodeRunnerFile.py
@@ -0,0 +1,20 @@
+
+# my_email = "4vm24is048@gmail.com"
+# password = "pzkpyuklywkwvfnd"
+
+# with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+#     connection.starttls()
+
+#     connection.login(user=my_email, password=password)
+
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="yashwantyash067@gmail.com",
+#         msg="Subject: Test Email\n\nhello bro!!!!!!!"
+#     )
+
+# now=dt.datetime.now()
+# today=now.weekday()
+# print(today)
+# # date_of_birth =dt.datetime(year=2005, month=12, day=2)
+# # print(date_of_birth)
